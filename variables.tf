a6d06cfe5be3b3a8b458fa125ed382831103624a
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-06-22 13:45:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
 
+variable "sampleObject" {
+  type = object({size=number,tag1=string,tag2=string, isDev=bool})
+  default = {
+    size: 80
+    tag1= "Object Volume Tag", 
+    tag2 = "Third Volume Tag"
+    isDev = true
+  }
+}
