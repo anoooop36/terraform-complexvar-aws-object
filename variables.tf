f2d7f23eac9622152de518460c52ec89494d824f
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-22 21:41:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,16 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+ type    = string
+ default = "us-east-1"
+}
 
 variable "sampleObject" {
-  type = object({size=number,tag1=string,tag2=string, isDev=bool})
+  type    = object({size=number, tag=string, isDev=bool})
   default = {
-    size: 40
-    tag1= "Iac Object Volume Tag", 
-    tag2 = "Third Volume Tag"
+    size  : 40
+    tag   = "Dev ebs volume", 
     isDev = true
   }
 }
